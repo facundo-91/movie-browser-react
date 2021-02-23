264ec95828a32e0e3485052600f4cf249cdbc0e6
Facundo Benitez <facundob91@gmail.com>
2021-02-23 16:08:19 -0300

    add movie trailer section to MovieInfo page

---
+++ src/components/MovieTrailer.js
@@ -0,0 +1,21 @@
+const MovieTrailer = ({ movieData }) => {
+	return (
+		<div className='px-3 py-6 mx-4 bg-gray-custom xl:mx-0'>
+			<h1 className='mx-1 text-xl font-bold md:text-2xl'>Trailer</h1>
+			<div className='relative h-0 pb-9/16'>
+				<iframe
+					width='560'
+					height='315'
+					className='absolute inset-0 w-full h-full my-2'
+					src={`https://www.youtube.com/embed/${movieData.videos.results[0].key}`}
+					frameborder='0'
+					allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+					allowfullscreen
+					title='Movie Trailer'></iframe>
+			</div>
+			<hr className='w-full h-16 border-b-2 border-gray-input-text'></hr>
+		</div>
+	);
+};
+
+export default MovieTrailer;
--- src/pages/MovieInfo.js
+++ src/pages/MovieInfo.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
-import ActorsInfo from '../components/ActorsInfo';
 import MainMovieInfo from '../components/MainMovieInfo';
+import ActorsInfo from '../components/ActorsInfo';
+import MovieTrailer from '../components/MovieTrailer';
 
 const MovieInfo = ({ match }) => {
 	// Hooks
@@ -13,7 +14,7 @@ const MovieInfo = ({ match }) => {
 		const getMovieInfo = async () => {
 			try {
 				setLoading(true);
-				const url = `https://api.themoviedb.org/3/movie/${match.params.id}?api_key=${process.env.REACT_APP_TMDB_API}&append_to_response=release_dates,credits`;
+				const url = `https://api.themoviedb.org/3/movie/${match.params.id}?api_key=${process.env.REACT_APP_TMDB_API}&append_to_response=release_dates,credits,videos`;
 				const response = await fetch(url);
 				const responseJson = await response.json();
 				setMovieInfo(responseJson);
@@ -35,6 +36,7 @@ const MovieInfo = ({ match }) => {
 				<div className='w-full max-w-lg py-20 mx-auto md:max-w-6xl'>
 					<MainMovieInfo movieData={movieInfo} />
 					<ActorsInfo movieData={movieInfo} />
+					<MovieTrailer movieData={movieInfo} />
 				</div>
 			</div>
 		)
