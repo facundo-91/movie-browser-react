2dd6fe22c70c11765e5b770aab0ca09d3a934355
Facundo Benitez <facundob91@gmail.com>
2021-02-25 23:44:47 -0300

    more fixes on Recommendations section

--- src/components/MovieRecommendations.js
+++ src/components/MovieRecommendations.js
@@ -4,7 +4,7 @@ const MovieRecommendations = ({ movieData }) => {
 	return (
 		<div className='px-3 py-6 mx-4 bg-gray-custom xl:mx-0'>
 			<h1 className='mx-1 text-xl font-bold md:text-2xl'>Recommendations</h1>
-			{!movieData.recommendations.results.length === 0 ? (
+			{movieData.recommendations.results.length !== 0 ? (
 				<div className='flex flex-wrap mt-4'>
 					{movieData.recommendations.results.map((movie) => {
 						return <MovieCard key={movie.id} id={movie.id} poster={movie.poster_path} />;
