d1be6ce3de68c3bac7e54e6e676458bc077416a5
Facundo Benitez <facundob91@gmail.com>
2021-03-02 20:47:24 -0300

    fix images on ActorsInfo section

--- src/components/ActorsInfo.js
+++ src/components/ActorsInfo.js
@@ -50,10 +50,19 @@ const ActorsInfo = ({ movieData }) => {
 					movieData.credits.cast.slice(0, 6).map((person) => {
 						return (
 							<div className='w-1/3 px-1 my-2 md:my-4 md:w-1/6' key={person.id}>
-								<img
-									src={`https://image.tmdb.org/t/p/w185${person.profile_path}`}
-									alt='Actor Profile'
-									className='w-full h-auto mx-0 my-2 rounded'></img>
+								{person.profile_path ? (
+									<img
+										src={`https://image.tmdb.org/t/p/w185${person.profile_path}`}
+										alt='Actor Profile'
+										className='w-full h-auto mx-0 my-2 rounded'></img>
+								) : (
+									<div className='flex justify-center align-middle'>
+										<img
+											src='https://icongr.am/fontawesome/user.svg?size=128&color=141414'
+											alt='Crew Profile'
+											className='w-full my-2 rounded py-1/4 bg-white-custom'></img>
+									</div>
+								)}
 								<p className='text-sm font-bold md:text-base'>{person.name}</p>
 								<p className='text-xs md:text-sm'>{person.character}</p>
 							</div>
@@ -63,10 +72,19 @@ const ActorsInfo = ({ movieData }) => {
 					movieData.credits.cast.slice(0, 18).map((person) => {
 						return (
 							<div className='w-1/3 px-1 my-2 md:my-4 md:w-1/6' key={person.id}>
-								<img
-									src={`https://image.tmdb.org/t/p/w185${person.profile_path}`}
-									alt='Actor Profile'
-									className='w-full h-auto mx-0 my-2 rounded'></img>
+								{person.profile_path ? (
+									<img
+										src={`https://image.tmdb.org/t/p/w185${person.profile_path}`}
+										alt='Actor Profile'
+										className='w-full h-auto mx-0 my-2 rounded'></img>
+								) : (
+									<div className='flex justify-center align-middle'>
+										<img
+											src='https://icongr.am/fontawesome/user.svg?size=128&color=141414'
+											alt='Crew Profile'
+											className='w-full my-2 rounded py-1/4 bg-white-custom'></img>
+									</div>
+								)}
 								<p className='text-sm font-bold md:text-base'>{person.name}</p>
 								<p className='text-xs md:text-sm'>{person.character}</p>
 							</div>
@@ -77,10 +95,19 @@ const ActorsInfo = ({ movieData }) => {
 						{movieData.credits.cast.map((person) => {
 							return (
 								<div className='w-1/3 px-1 my-2 md:my-4 md:w-1/6' key={person.id}>
-									<img
-										src={`https://image.tmdb.org/t/p/w185${person.profile_path}`}
-										alt='Actor Profile'
-										className='w-full h-auto mx-0 my-2 rounded'></img>
+									{person.profile_path ? (
+										<img
+											src={`https://image.tmdb.org/t/p/w185${person.profile_path}`}
+											alt='Actor Profile'
+											className='w-full h-auto mx-0 my-2 rounded'></img>
+									) : (
+										<div className='flex justify-center align-middle'>
+											<img
+												src='https://icongr.am/fontawesome/user.svg?size=128&color=141414'
+												alt='Crew Profile'
+												className='w-full my-2 rounded py-1/4 bg-white-custom'></img>
+										</div>
+									)}
 									<p className='text-sm font-bold md:text-base'>{person.name}</p>
 									<p className='text-xs md:text-sm'>{person.character}</p>
 								</div>
