4d63ca071653f8d30446466949a8b69461d23fac
Facundo Benitez <facundob91@gmail.com>
2021-04-05 15:38:42 -0300

    fix for deploy in gh pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { MoviesProvider } from './contexts/MoviesContext';
 import { FirestoreProvider } from './contexts/FirestoreContext';
